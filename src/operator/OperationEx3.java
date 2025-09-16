04878cfef553bdf9d008ac01bf3ec74e9b79699c
hyejin0916 <rkdgpwlsz@naver.com>
2025-09-16 21:20:57 +0900

    section4 - operator

---
+++ src/operator/OperationEx3.java
@@ -0,0 +1,10 @@
+package operator;
+
+public class OperationEx3 {
+
+    public static void main(String[] args) {
+        int score = 79;
+
+         System.out.println(80 <= score && score <= 100);
+    }
+}
